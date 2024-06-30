aaf1bee5061851cf76d1cbc9813ee40b901cfa42
Julia <klimuk.julia@gmail.com>
2024-06-30 10:11:37 +0300

    fixes

--- src/tests/sortingCheck.spec.js
+++ src/tests/sortingCheck.spec.js
@@ -1,21 +1,29 @@
-// test file (e.g., inventory.test.js)
 const { expect } = require('@playwright/test');
 const { test } = require('../fixture');
 
-test.describe('Test 1', () => {
-    test('Sort items on Inventory page', async ({ loginPage, inventoryPage, shopingCartPage }) => {
+test.describe('Inventory Page Sorting', () => {
+    test.beforeEach(async ({ loginPage }) => {
         await loginPage.navigate();
         await loginPage.performLogin('standard_user', 'secret_sauce');
+    });
 
-       await inventoryPage.sortItems('az');
-        let itemNames = await inventoryPage.getItemNames();
-        let sortedItemNames = [...itemNames].sort();
+    test('Sort items in ascending order (A-Z)', async ({ inventoryPage }) => {
+        await inventoryPage.sortItems('az');
+        const itemNames = await inventoryPage.getItemNames();
+        const sortedItemNames = itemNames.sort();
         expect(itemNames).toEqual(sortedItemNames);
+    });
 
-       await inventoryPage.sortItems('za');
-        itemNames = await inventoryPage.getItemNames();
-        sortedItemNames = [...itemNames].sort().reverse();
+    test('Sort items in descending order (Z-A)', async ({ inventoryPage }) => {
+        await inventoryPage.sortItems('za');
+        const itemNames = await inventoryPage.getItemNames();
+        const sortedItemNames = itemNames.sort().reverse();
         expect(itemNames).toEqual(sortedItemNames);
+    });
 
+    test('Check item names without sorting', async ({ inventoryPage }) => {
+        const itemNames = await inventoryPage.getItemNames();
+        const sortedItemNames = itemNames.sort();
+        expect(itemNames).not.toEqual(sortedItemNames);
     });
 });
